a1a05805a9095f1b5868328a034705af823c90e2
Abdullah <abdullahsajjad33@gmail.com>
2021-07-29 23:33:55 +0500

    added settings class

---
+++ admin/includes/class-breaking-news-settings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Main Admin Settings Class.
+*
+* This file is used to define admin settings using settings API.
+*
+* @link       https://github.com/abdullahsajjad
+* @since      0.0.1
+*
+* @package    Breaking_News
+* @subpackage Breaking_News/admin/includes
+*/
+
+// If this file is called directly, abort.
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Main Admin Settings Class
+ *
+ * this Class is used to define admin settings
+ *
+ * @since 0.0.1
+ */
+class Breaking_News_Settings {
+
+}

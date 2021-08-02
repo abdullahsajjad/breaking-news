c522bd40fdb6f148b29a8c0dff8a556b720f8f93
Abdullah <abdullahsajjad33@gmail.com>
2021-08-02 18:07:11 +0500

    remove plugin data on uninstall if the enabled from settings

--- uninstall.php
+++ uninstall.php
@@ -20,7 +20,7 @@
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
  * @link       https://github.com/abdullahsajjad
- * @since      1.0.0
+ * @since      0.0.3
  *
  * @package    Breaking_News
  */
@@ -29,3 +29,61 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+
+if ( ! function_exists( 'bn_delete_all_options' ) ) {
+
+	/**
+	 * Deletes plugin data in options table
+	 *
+	 * @since 0.0.3
+	 */
+	function bn_delete_all_options() {
+		$options_arr = [
+			'bn_settings',
+		];
+		foreach ( $options_arr as $option ) {
+			delete_option( $option );
+		}
+	}
+
+} // endif
+
+if ( ! function_exists( 'bn_is_data_removal_allowed' ) ) {
+
+	/**
+	 * checks if allowed removing data on plugin uninstall
+	 *
+	 * @return bool
+	 * @since 0.0.3
+	 */
+	function bn_is_data_removal_allowed() {
+		$general_options = get_option( 'bn_settings' );
+		if ( ! empty( $general_options['remove_data'] ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+} // endif
+
+if ( ! function_exists( 'bn_uninstall_driver' ) ) {
+
+	/**
+	 * Driver Function
+	 *
+	 * @since 0.0.3
+	 */
+	function bn_uninstall_driver() {
+		if ( bn_is_data_removal_allowed() === false ) { //if not allowed return
+			return;
+		}
+
+		bn_delete_all_options();
+	}
+
+} // endif
+
+
+bn_uninstall_driver();

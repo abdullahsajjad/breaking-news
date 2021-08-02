a8c6193a1b7c87a90e1046f4a30cba3dbdf6dd89
Abdullah <abdullahsajjad33@gmail.com>
2021-08-02 15:48:32 +0500

    added event expiry

--- public/class-breaking-news-public.php
+++ public/class-breaking-news-public.php
@@ -83,6 +83,11 @@ class Breaking_News_Public {
 			return;
 		}
 
+		// checking expiry
+		if( $this->check_breaking_news_expiry( $bn_settings['post_id'] ) === TRUE ) {
+            return;
+		}
+
 		$title = get_post_meta( $bn_settings['post_id'], 'bn-custom-title', true );
 		if ( $title === '' || $title === false ) {
 			$title = get_the_title( $bn_settings['post_id'] );
@@ -92,7 +97,7 @@ class Breaking_News_Public {
 		$txt_color = $bn_settings['text_color'];
 		$html      = "<div class='breaking news' style='padding: 10px; text-transform: capitalize;text-align: center; background-color:$bg_color; color:$txt_color;'>{$bn_settings['news_title']}&nbsp;<a style='color:$txt_color;' href=" . get_the_permalink( $bn_settings['post_id'] ) . ">$title</a></div>";
 
-		?>
+ 		?>
         <script>
             jQuery( document ).ready( function (){
                 let display = false;
@@ -111,4 +116,34 @@ class Breaking_News_Public {
 		<?php
 	}
 
+	/**
+	 * Checks The Breaking News Expiry and unset
+	 * the breaking news from options
+	 *
+	 * @param $post_ID
+	 *
+	 * @return boolean
+	 *
+	 * @since 0.0.1
+	 */
+	protected function check_breaking_news_expiry( $post_ID ): bool {
+		$is_expiry = get_post_meta( $post_ID, 'bn-is-expiry', true );
+		if( empty( $is_expiry ) ) {
+		    return FALSE;
+        }
+
+		$expiry_date_time = get_post_meta( $post_ID, 'bn-expiry-date-time', true );
+		$current_time     = strtotime( current_time( 'mysql' ) );
+
+		if ( $current_time < $expiry_date_time ) {   // expire time is less than current time
+			return false;
+		}
+
+		// unset breaking news
+		$bn_settings = get_option( 'bn_settings' );
+		unset( $bn_settings['post_id'] );
+		update_option( 'bn_settings', $bn_settings );
+
+		return true;
+	}
 }
